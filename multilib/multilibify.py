4410d2887639a40b981613f02a77293ec923fa4e
gasinvein <gasinvein@gmail.com>
2020-09-17 18:25:31 +0300

    multilib: Reduce script complexity

--- multilib/multilibify.py
+++ multilib/multilibify.py
@@ -48,32 +48,28 @@ def save_dict_file(dict_data, dict_file):
             raise ValueError(f"Unknown format of {dict_file}")
         writer(dict_data, f)
 
-def merge_dicts(base_dict, update_dict, append_list=False):
+def merge_dicts(base_dict, update_dict):
     merged_dict = copy.deepcopy(base_dict)
     for upd_key, upd_val in update_dict.items():
         if isinstance(merged_dict.get(upd_key), dict) and isinstance(upd_val, dict):
             merged_dict[upd_key] = merge_dicts(merged_dict[upd_key], upd_val)
-        elif append_list and isinstance(merged_dict.get(upd_key), list) and isinstance(upd_val, list):
-            merged_dict[upd_key] += upd_val
         else:
             merged_dict[upd_key] = upd_val
     return merged_dict
 
-def multilibify(holder_object, holder_file, base_dir=None, add=None, props=None):
-    if add is None:
-        add = {v: True for v in VARIANTS.keys()}
-    if props is None:
-        props = {v: None for v in VARIANTS.keys()}
+def multilibify(holder_object, holder_file, base_dir=None, variants=None):
     holder_dir = os.path.dirname(holder_file)
     if base_dir is None:
         base_dir = holder_dir
 
-    default_multilib = holder_object.pop(MULTILIB_PROP, True)
-    for v in VARIANTS.keys():
-        if props[v] is None:
-            props[v] = holder_object.pop(VARIANTS[v]["prop"], {})
-        if isinstance(props[v], str):
-            props[v] = load_dict_file(os.path.join(holder_dir, props[v]))
+    if variants is None:
+        variants = {}
+        for v, opts in VARIANTS.items():
+            variants[v] = holder_object.pop(opts["prop"], {})
+            if isinstance(variants[v], str):
+                variants[v] = load_dict_file(os.path.join(holder_dir, variants[v]))
+
+    holder_multilib = holder_object.pop(MULTILIB_PROP, True)
     orig_modules = holder_object["modules"]
     modules = []
 
@@ -89,24 +85,15 @@ def multilibify(holder_object, holder_file, base_dir=None, add=None, props=None)
                 source_path = os.path.join(os.path.dirname(module_file), source["path"])
                 source["path"] = os.path.relpath(source_path, base_dir)
 
-        if not orig_module.pop(MULTILIB_PROP, default_multilib):
+        if not orig_module.pop(MULTILIB_PROP, holder_multilib):
             modules.append(orig_module)
             continue
 
-        module_props = {}
-        for v in VARIANTS.keys():
-            module_props[v] = orig_module.pop(VARIANTS[v]["prop"], {})
-
-        for v in VARIANTS.keys():
-            if not add[v]:
-                continue
-            new_module = merge_dicts(orig_module, props[v])
-            new_module = merge_dicts(new_module, module_props[v])
-            if "name-suffix" in VARIANTS[v]:
-                new_module["name"] = "{0}{1}".format(orig_module["name"], VARIANTS[v]["name-suffix"])
+        for v, props in variants.items():
+            new_module = merge_dicts(orig_module, props)
+            new_module["name"] = "{0}{1}".format(orig_module["name"], VARIANTS[v].get("name-suffix", ""))
             if "modules" in new_module:
-                submodule_add = {i: i == v for i in VARIANTS.keys()}
-                new_module = multilibify(new_module, module_file, base_dir, props=props, add=submodule_add)
+                new_module = multilibify(new_module, module_file, base_dir, variants={v: props})
             modules.append(new_module)
 
     holder_object["modules"] = modules
